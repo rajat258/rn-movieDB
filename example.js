d7c929299fe7abeb96d2a8e80db57a8680235ea9
Rajat Nanavati <rajatnanavati@Harry-Potter.local>
2024-05-27 01:08:12 +0530

    testing commit

---
+++ example.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const example = () => {
+    
+    return (
+        <View>
+        <Text>example</Text>
+        </View>
+
+  )
+}
+
+export default example
+
+const styles = StyleSheet.create({})
